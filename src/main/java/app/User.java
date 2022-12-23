e70a5c95aec1e2c6f8879349bae006c3fe2f95f2
=Benoit Marchadier <marchadier.benoit@gmail.com>
2022-12-23 14:49:52 +0100

    @GeneratedValue (strategy = GenerationType.AUTO)

--- src/main/java/app/User.java
+++ src/main/java/app/User.java
@@ -1,12 +1,13 @@
 package app;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.AUTO)
     private Integer id;
     private String username;
     private String email;
