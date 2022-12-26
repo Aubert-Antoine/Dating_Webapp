014c63575cc41952c1097a4c7900e03c6dd2f162
Antoine Aubert <antoine0aubert@edu.esiee.fr>
2022-12-26 10:09:50 +0100

    openAI lib accepted

--- src/main/resources/app/static/components/Lorem.js
+++ src/main/resources/app/static/components/Lorem.js
@@ -1,11 +1,15 @@
 function lorem () {
     return (
         <nav>
-            <ul className="Nav__bar_ul">
-                <li className="Nav__bar_li"><a href="../index.html"><b>Discover</b></a></li>
-                <li className="Nav__bar_li"><a href="../index.html"><b>Chat</b></a></li>
-                <li className="Nav__bar_li"><a href="../index.html"><b>Profile</b></a></li>
-            </ul>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <p>
+                Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor hodor; hodor hodor - hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor; hodor hodor; hodor hodor hodor! Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor...
+            </p>
+            <p>
+                Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag. Brunch raclette vexillologist post-ironic glossier ennui XOXO mlkshk godard pour-over blog tumblr humblebrag. Blue bottle put a bird on it twee prism biodiesel brooklyn. Blue bottle ennui tbh succulents.
+            </p>
         </nav>
     );
 }
