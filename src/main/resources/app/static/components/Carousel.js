58b6257ec8dd0bcc372ca58973276a3f9d446c29
Antoine Aubert <antoine0aubert@edu.esiee.fr>
2022-12-28 15:36:09 +0100

    des trucs

--- src/main/resources/app/static/components/Carousel.js
+++ src/main/resources/app/static/components/Carousel.js
@@ -48,14 +48,23 @@ class Carousel extends Component {
         alert("handle like from"+ +this.state.user_id+" to " +user_id  )
         fetch('/likes?from='+this.state.user_id+"&to="+user_id)
         // Handle the "like" action for the current profile
+        this.setState(prevState => ({
+            currentIndex: (prevState.currentIndex + 1) % prevState.profiles.length
+        }));
+        this.setState({currentprofile: this.state.profiles[this.state.currentIndex]})
     }
 
 
 
 
     handleDislike = (user_id) => {
-        alert("handle dislike")
-        // Handle the "dislike" action for the current profile
+        alert("handle like from"+ +this.state.user_id+" to " +user_id  )
+        fetch('/likes?from='+this.state.user_id+"&to="+user_id)
+        // Handle the "like" action for the current profile
+        this.setState(prevState => ({
+            currentIndex: (prevState.currentIndex + 1) % prevState.profiles.length
+        }));
+        this.setState({currentprofile: this.state.profiles[this.state.currentIndex]})
     }
 
     handleNext = () => {
@@ -75,60 +84,61 @@ class Carousel extends Component {
     render() {
         //const { currentIndex, profiles } = this.state;
         //const currentProfile = profiles[currentIndex];
+
+        //<button onClick={this.handlePrev}>&lt;</button>
+        //<button onClick={this.handleNext}>&gt;</button>
         const currentProfile =   this.state.currentprofile;
         const currentIndex = this.state.currentIndex;
         const activepage = this.state.activepage;
-        if(activepage === "Discover") {
 
 
+        if(activepage === "Discover")
+        {
+        return (
+                <div>
+                    <nav>
+                        <ul className="Nav__bar_ul">
+                            <button className="button_navbar" onClick={() =>this.changeActivePage("Discover")}>Discover</button>
+                            <button className="button_navbar" onClick={() =>this.changeActivePage("Profile")}>Profile</button>
+                        </ul>
+                    </nav>
+
+                     <body className="body">
+                         <div className="carousel__item">
+                             <img src={currentProfile.picture} alt="Profile picture" />
+                             <div className="carousel__item__info">
+                                 <h1>{currentProfile.firstName} {currentProfile.lastName}</h1>
+                                 <p>{currentProfile.description}</p>
+                                 <p>Location: {currentProfile.city}</p>
+                                 <p>Date of birth: {currentProfile.birthDate}</p>
+                                 <p>Gender: {currentProfile.male}</p>
+                                 <p>id: {currentProfile.id}</p>
+                             </div>
+                             <button className="button_carousel-like" onClick={() =>this.handleLike(currentProfile.id)}>Like</button>
+                             <button className="button_carousel-dislike" onClick={() =>this.handleDislike (currentProfile.id)}>Dislike</button>
+                        </div>
+                    </body>
+                </div>
+            );
+        }
+        else if (activepage === "Profile")
+        {
         return (
             <div>
                 <nav>
-                    <ul className="Nav__bar_ul">
-                        <button className="button_navbar" onClick={() =>this.changeActivePage("Discover")}>Discover</button>
-                        <button className="button_navbar" onClick={() =>this.changeActivePage("Profile")}>Profile</button>
-                    </ul>
+                <ul className="Nav__bar_ul">
+                    <button className="button_navbar" onClick={() =>this.changeActivePage("Discover")}>Discover</button>
+                    <button className="button_navbar" onClick={() =>this.changeActivePage("Profile")}>Profile</button>
+                </ul>
                 </nav>
-                <div> User profile </div>
 
-                 <div className="carousel">
-
-                    <button onClick={this.handlePrev}>&lt;</button>
-                        <div className="carousel__item">
-                            <img src={currentProfile.picture} alt="Profile picture" />
-                            <div className="carousel__item__info">
-                                <h1>{currentProfile.firstName} {currentProfile.lastName}</h1>
-                                <p>{currentProfile.description}</p>
-                                <p>Location: {currentProfile.city}</p>
-                                <p>Date of birth: {currentProfile.dob}</p>
-                                <p>Gender: {currentProfile.gender}</p>
-                                <p>id: {currentProfile.id}</p>
-                            </div>
-                            <button onClick={() =>this.handleLike(currentProfile.id)}>Like</button>
-                            <button onClick={() =>this.handleDislike (currentProfile.id)}>Dislike</button>
-                        </div>
-                        <button onClick={this.handleNext}>&gt;</button>
-                    </div>
-
-
-
+                <body>
+                <h1>test a la place de profil </h1>
+                </body>
             </div>
-
-        );
+            );
         }
-        else
-        {
-            return (<div>
-                    <nav>
-                    <ul className="Nav__bar_ul">
-                        <button onClick={() =>this.changeActivePage("Discover")}>Discover</button>
-                        <button onClick={() =>this.changeActivePage("Profile")}>Profile</button>
-                    </ul>
-                    </nav>
-                something else </div>)
-        }
-    }
-
-}
+    }//render()
+}//class Carousel
 
 ReactDOM.render(<Carousel/>, document.getElementById("carousel"))
